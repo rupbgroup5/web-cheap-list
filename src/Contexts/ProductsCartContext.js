c678253d2beeb2f4ceb8ff47b2e1df4f83133819
Yogev Strauber <yogev.strauber@gmail.com>
2020-05-19 19:33:24 +0300

    some work in mylist in super market

--- src/Contexts/ProductsCartContext.js
+++ src/Contexts/ProductsCartContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-
+import '../Styles/SuperMarketListStyle.css'
 
 export const ProductsCartContext = createContext();
 
--- src/Pages/SuperMarketList.jsx
+++ src/Pages/SuperMarketList.jsx
@@ -1,25 +1,83 @@
-import React, { useContext } from 'react';
-import { withRouter } from 'react-router-dom';
+import React from 'react' //, { useContext }
+import { withRouter } from 'react-router-dom'
+import { FixedSizeList as List } from 'react-window'; //https://react-window.now.sh/#/examples/list/fixed-size
+import Checkbox from '@material-ui/core/Checkbox';
+
+import Button from '@material-ui/core/Button';
+
 
 
 //Context Api:
-import { ProductsCartContext } from "../Contexts/ProductsCartContext";
+//import { ProductsCartContext } from "../Contexts/ProductsCartContext";
+
 
 
 const SuperMarketList = () => {
-    const  { productCart } = useContext(ProductsCartContext); //SetProductCart
+  //const  { productCart } = useContext(ProductsCartContext); //SetProductCart
+  //temp:
+  let productCart = ["גיל", "גבינה לבנה 5%", "קוטג'", "חומוס", "חלב", "לחם", "גיל", "גבינה לבנה 5%", "קוטג'", "חומוס", "חלב", "לחם"];
 
-    return (
-        <div>
-            {console.log(productCart)}
-           
-            <ul>
-            {productCart.map((p, index)=>{
-                return <li key={index}>{p.product_name}</li>
-            })}
-            </ul>
-        </div>
-    )
+  const Row = ({ index, style }) => (
+
+    <div style={style}>
+      {productCart[index]}
+      <Checkbox
+        color="primary"
+
+      />
+
+    </div>
+
+  );
+
+  return (
+    <div id='lists-container'>
+
+      <h3 id="list-header">הרשימה שלי בסופר</h3>
+      <List
+        height={220}
+        itemCount={productCart.length}
+        itemSize={30}
+        width={300}
+      >
+        {Row}
+      </List>
+
+      <div id='taken-list'>
+        <h5>בעגלה</h5>
+        <List
+          height={130}
+          itemCount={productCart.length}
+          itemSize={30}
+          width={200}
+        >
+          {Row}
+        </List>
+
+      </div>
+
+      
+      <div id='untaken-list'>
+        <h5>מוצרים שלא לקחתי</h5>
+        <List
+          height={130}
+          itemCount={productCart.length}
+          itemSize={30}
+          width={200}
+        >
+          {Row}
+        </List>
+
+      </div>
+
+      <div id='finishBtn'>
+        <Button variant="contained" color="primary" >
+        סיימתי קנייה
+        </Button>
+      </div>
+          </div>
+
+  )
 }
 
 export default withRouter(SuperMarketList);
