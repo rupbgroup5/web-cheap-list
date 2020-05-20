18473f1923bb289ea684f57fe943cfc82d78c903
Yogev Strauber <yogev.strauber@gmail.com>
2020-05-20 20:01:52 +0300

    some more work on supermarket list

--- src/Pages/SuperMarketList.jsx
+++ src/Pages/SuperMarketList.jsx
@@ -1,4 +1,4 @@
-import React from 'react' //, { useContext }
+import React, { useState } from 'react' //, { useContext }
 import { withRouter } from 'react-router-dom'
 import { FixedSizeList as List } from 'react-window'; //https://react-window.now.sh/#/examples/list/fixed-size
 import Checkbox from '@material-ui/core/Checkbox';
@@ -15,19 +15,59 @@ import Button from '@material-ui/core/Button';
 const SuperMarketList = () => {
   //const  { productCart } = useContext(ProductsCartContext); //SetProductCart
   //temp:
-  let productCart = ["גיל", "גבינה לבנה 5%", "קוטג'", "חומוס", "חלב", "לחם", "גיל", "גבינה לבנה 5%", "קוטג'", "חומוס", "חלב", "לחם"];
+  const [productCart, SetProductCart ] = useState(["גבינה לבנה 5%", "קוטג'", "חומוס", "חלב", "לחם"]);
+  const [takenArray, SettakenArray] = useState([]);
+  const [unTakenArray, SetUnTakenArray] = useState([]);
 
-  const Row = ({ index, style }) => (
+  const HandleChecked = (productName, productIndex) => {
+    let temp = [];
+    for (let i = 0; i < productCart.length; i++){
+      if(i !== productIndex){
+        temp.push(productCart[i]);
+      }
+    } 
+    SetProductCart(temp);
+    takenArray.push(productName);
 
+  }
+  const HandleUnChecked = (productName, productIndex) =>{
+    let temp = [];
+    for (let i = 0; i < takenArray.length; i++){
+      if(i !== productIndex){
+        temp.push(takenArray[i]);
+      }
+    } 
+    SettakenArray(temp);
+    productCart.push(productName);
+  }
+
+  const ProductInCart = ({ index, style }) => (
     <div style={style}>
       {productCart[index]}
       <Checkbox
         color="primary"
-
+        onClick={()=>{HandleChecked(productCart[index], index)}}
       />
-
     </div>
+  );
 
+  const ProductInTaken = ({ index, style }) => (
+    <div style={style}>
+      {takenArray[index]}
+      <Checkbox
+        color="primary"
+        checked={true}
+        onClick={()=>{HandleUnChecked(takenArray[index], index)}}
+      />
+    </div>
+  );
+  const ProductInUnTaken = ({ index, style }) => (
+    <div style={style}>
+      {unTakenArray[index]}
+      <Checkbox
+        color="primary"
+      />
+    </div>
   );
 
   return (
@@ -40,34 +80,36 @@ const SuperMarketList = () => {
         itemSize={30}
         width={300}
       >
-        {Row}
+        {ProductInCart}
       </List>
 
       <div id='taken-list'>
-        <h5>בעגלה</h5>
+        <h5>נכנס לעגלה</h5>
+        {takenArray.length === 0 ? <p>העגלה עדיין ריקה</p>: 
         <List
-          height={130}
-          itemCount={productCart.length}
+          height={100}
+          itemCount={takenArray.length}
           itemSize={30}
           width={200}
         >
-          {Row}
+         {ProductInTaken}
         </List>
-
+      }
       </div>
 
       
       <div id='untaken-list'>
         <h5>מוצרים שלא לקחתי</h5>
+        {takenArray.length === 0 ? <p>רשימה זו ריקה</p>: 
         <List
-          height={130}
-          itemCount={productCart.length}
+          height={100}
+          itemCount={unTakenArray.length}
           itemSize={30}
           width={200}
         >
-          {Row}
+          {ProductInUnTaken}
         </List>
-
+}
       </div>
 
       <div id='finishBtn'>
