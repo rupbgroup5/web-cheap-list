a19df1cdb457b1d79c020b78af2fdad043e0d4f7
Yogev Strauber <yogev.strauber@gmail.com>
2020-07-22 11:53:45 +0300

    error boundary module
    started to worlk on the error boundary module but not finish it yet

--- src/Components/ErrorMessage.js
+++ src/Components/ErrorMessage.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from 'react'
 import { withRouter, useHistory } from 'react-router-dom'
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-
-
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
 
 const useStyles = makeStyles((theme) => ({
     root: {
